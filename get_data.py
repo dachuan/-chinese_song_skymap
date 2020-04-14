ef056c12569232905b1b43fb31e3a6f6a2939de5
dcthe <tianiao@126.com>
2020-04-14 19:40:36 +0800

    tbd

--- get_data.py
+++ get_data.py
@@ -9,9 +9,13 @@ with load.open('/anaconda3/envs/astro/lib/python3.8/site-packages/skyfield/data/
     df = hipparcos.load_dataframe(f)
 
 print("hip data :",df.shape)
-df.to_csv('./res/hip_all.csv')
+#df.to_csv('./res/hip_all.csv')
 
 mag90 = df[df['magnitude']<= 9.0]
 print("mag under 9.0 :",mag90.shape)
 
-mag90.to_csv('./res/hip_mag90.csv')
+#mag90.to_csv('./res/hip_mag90.csv')
+
+
+#axis_star = Star.from_dataframe(df.iloc[62518])
+
