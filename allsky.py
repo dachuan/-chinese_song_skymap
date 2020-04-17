c0cc1415cfd96e65eefc16a47de84ce6b2ad7a0f
dcthe <tianiao@126.com>
2020-04-17 14:56:55 +0800

    drop nan

--- allsky.py
+++ allsky.py
@@ -14,70 +14,50 @@ from skyfield.units import Angle
 # load hip star data
 asterisms= pd.read_csv('./res/song_con.txt',header=None) # song star data
 asterisms.columns=['constellation','num_pairs','stars','name','ras','decs','xingxiu','mags']
-print(asterisms.shape)
-print(asterisms.loc[0,'name'])
-
-
-# Ursa minor
-#hips_umi=[11767, 85822, 85822, 82080, 82080, 77055, 77055, 79822, 79822, 75097, 75097, 72607, 72607, 77055]
-
-# construct stars 
-#umi_stars = [Star.from_dataframe(df.loc[v]) for v in np.unique(hips_umi)]
 
 # time scale is important to calculate
-ts = load.timescale()
-t_song = ts.utc(1000,1,1,20,0) # song dynasty
-t_now = ts.now()
-
-planets = load('de421.bsp')
-earth = planets['earth']
-
-kaifeng = earth + Topos(longitude_degrees=(114,30,0),
-                        latitude_degrees=(+34,8,0)) # north song capital
-
-"""
-ras_beiji=[]
-decs_beiji=[]
-ras_umi = []
-decs_umi = []
-ras_beidou = []
-decs_beidou = []
-ras_gouchen = []
-decs_gouchen = []
-
-for star in beiji_stars:
-    astrometric = kaifeng.at(t_now).observe(star)
-    apparent = astrometric.apparent()
-    ra,dec,_ = apparent.radec(epoch=t_song)
-    ras_beiji.append(ra.radians)
-    decs_beiji.append(dec.degrees)
-
-
-for star in umi_stars:
-    astrometric = kaifeng.at(t_now).observe(star)
-    apparent = astrometric.apparent()
-    ra,dec,_ = apparent.radec(epoch=t_song)
-    ras_umi.append(ra.radians)
-    decs_umi.append(dec.degrees)
-
-for star in beidou_stars:
-    astrometric = kaifeng.at(t_now).observe(star)
-    apparent = astrometric.apparent()
-    ra,dec,_ = apparent.radec(epoch=t_song)
-    ras_beidou.append(ra.radians)
-    decs_beidou.append(dec.degrees)
-
-for star in gouchen_stars:
-    astrometric = kaifeng.at(t_now).observe(star)
-    apparent = astrometric.apparent()
-    ra,dec,_ = apparent.radec(epoch=t_song)
-    ras_gouchen.append(ra.radians)
-    decs_gouchen.append(dec.degrees)
+#ts = load.timescale()
+#t_song = ts.utc(1000,1,1,20,0) # song dynasty
+#t_now = ts.now()
+#
+#planets = load('de421.bsp')
+#earth = planets['earth']
+#
+#kaifeng = earth + Topos(longitude_degrees=(114,30,0),
+#                        latitude_degrees=(+34,8,0)) # north song capital
 
 fig = plt.figure()
 ax = fig.add_axes([0,0,1,1],polar=True)
 ax.set_theta_direction(-1)
-ax.set_ylim(-45, 45)
+ax.set_xticks([]) #no ticks
+ax.set_yticks([]) #no ticks
+ax.set_ylim(-45, 105) # -45, 45
+
+ax.set_theta_zero_location('N', offset=30)
+
+for index,row  in asterisms.iterrows():
+
+    # turn string into float-list
+    ras = row['ras'].replace('[','').replace(']','').split(',')
+    decs = row['decs'].replace('[','').replace(']','').split(',')
+
+    ras = [np.radians(float(i)) for i in ras]
+    decs = [float(i) for i in decs]
+
+    # plot stars
+    for ra,dec in zip(ras,decs):
+        ax.scatter(ra,45-dec,
+                color='grey',edgecolor='black',
+                s=1,
+                alpha=0.8,zorder=1)
+
+    # plot constellation lines
+    _decs = [45-i for i in decs]
+
+    for n in range(int(len(ras)/2)): #1-2
+        ax.plot(ras[n*2:(n+1)*2], _decs[n*2:(n+1)*2],
+                color='green', 
+                lw=1,
+                alpha=0.5,zorder=0)
 
 plt.show()
-"""
